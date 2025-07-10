d2c2e9a57f0647f32fe42d9cad1509200886155f
Marouane LOUKRISSI <marouaneloukrissi10@gmail.com>
2025-07-10 14:22:11 +0100

    initial commit

---
+++ OrienGO-Backend/src/main/java/com/example/oriengo/OrienGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oriengo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrienGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrienGoApplication.class, args);
+    }
+
+}
