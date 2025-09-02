498111297547be3c6f21cab2a63e469c691c717e
Marouane LOUKRISSI <148444621+MarouaneLoukrissi@users.noreply.github.com>
2025-09-02 21:13:40 +0100

    Remove hardcoded API key from gemini_key function
    Replace the hardcoded API key with an empty string.

--- OrienGo-AI/api/api_token.py
+++ OrienGo-AI/api/api_token.py
@@ -1,3 +1,3 @@
 def gemini_key():
     # ⚠️ Remplacez "VOTRE_CLE_API_ICI" par votre propre clé Gemini/API
-    return "AIzaSyBNsciEjd6kf-Tsnx_eud14iRs_F0j0MUc"#"AIzaSyCh1lCypB5_gtxxNSgRO8axIjWvsRIGikI"
+    return ""
